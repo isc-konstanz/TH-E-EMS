90a42dd657f1e535e21912de48f5d8e5ee75f4cf
Adrian Minde <Adrian_Minde@live.de>
2018-01-30 16:15:49 +0100

    Remove rolling average for circulation temperature listeners

--- projects/feature/circulation/src/main/java/de/thebox/control/feature/circulation/CirculationTemperatureListener.java
+++ projects/feature/circulation/src/main/java/de/thebox/control/feature/circulation/CirculationTemperatureListener.java
@@ -1,16 +1,10 @@
 package de.thebox.control.feature.circulation;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import de.thebox.control.core.data.Channel;
 import de.thebox.control.core.data.ChannelListener;
-import de.thebox.control.core.data.DoubleValue;
 import de.thebox.control.core.data.Value;
 
 public class CirculationTemperatureListener extends ChannelListener {
-
-	private final static int MOVING_AVG_SIZE = 6;
 
 	/**
 	 * Interface used to notify the {@link Circulation} 
@@ -27,9 +21,6 @@ public class CirculationTemperatureListener extends ChannelListener {
 
 	private final CirculationTemperature type;
 
-	private List<Double> temperatures = new LinkedList<Double>();
-	private double temperatureSum = -1;
-
 	public CirculationTemperatureListener(CirculationTemperatureCallbacks callbacks, CirculationTemperature type, Channel channel) {
 		super(channel);
 		
@@ -44,22 +35,7 @@ public class CirculationTemperatureListener extends ChannelListener {
 	@Override
 	public void onValueReceived(Value value) {
 		if (value != null) {
-			Double temperature = value.doubleValue();
-			
-			if (temperatureSum == -1) {
-				temperatureSum = temperature;
-				temperatures.add(temperature);
-			}
-			else {
-				while (temperatures.size() >= MOVING_AVG_SIZE) {
-					temperatureSum -= temperatures.remove(0);
-				}
-				temperatures.add(temperature);
-				temperatureSum += temperature;
-			}
-			
-			Double average = temperatureSum/temperatures.size();
-			callbacks.onTemperatureReceived(type, new DoubleValue(average, value.getTimestamp()));
+			callbacks.onTemperatureReceived(type, value);
 		}
 	}
 }

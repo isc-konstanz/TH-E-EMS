e72c1e16283fcc9708c3382e5973f2c69ce85228
Adrian Minde <Adrian_Minde@live.de>
2018-02-07 13:20:14 +0100

    Add sorting and interval to Schedule list

--- projects/core/src/main/java/de/thebox/control/core/schedule/Schedule.java
+++ projects/core/src/main/java/de/thebox/control/core/schedule/Schedule.java
@@ -1,41 +1,75 @@
 package de.thebox.control.core.schedule;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 import de.thebox.control.core.data.DoubleValue;
 import de.thebox.control.core.data.Value;
 
-public class Schedule extends ArrayList<Value> {
+public class Schedule extends LinkedList<Value> {
 	private static final long serialVersionUID = -96966430241693416L;
 
-	private final Value start;
-	private final long end;
+	private final Integer interval;
 
-	public Schedule(Value start, long end) {
-		this.start = start;
-		this.end = end;
+	public Schedule(int interval) {
+		this.interval = interval;
+	}
+
+	public Schedule() {
+		this.interval = null;
+	}
+
+	public Integer getInterval() {
+		return interval;
 	}
 
 	public long getStartTime() {
-		return start.getTimestamp();
+		Value start = peekFirst();
+		if (start != null) {
+			return start.getTimestamp();
+		}
+		return -1;
 	}
 
 	public long getEndTime() {
-		return end;
+		Value end = peekLast();
+		if (end != null) {
+			if (interval != null) {
+				return end.getTimestamp() + interval;
+			}
+			else if (size() > 1) {
+				return end.getTimestamp() + (end.getTimestamp() - getStartTime())/size();
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public boolean add(Value value) {
-		if (value.getTimestamp() > start.getTimestamp() &&
-				value.getTimestamp() < end) {
-			
-			return super.add(value);
+		if (value.getTimestamp() <= getStartTime() || value.getTimestamp() >= getEndTime() ||
+				(interval != null && (value.getTimestamp() - getStartTime()) % interval != 0)) {
+			return false;
 		}
-		return false;
+		return super.add(value);
 	}
 
 	public boolean add(long timestamp, double value) {
 		return add(new DoubleValue(value, timestamp));
 	}
 
+	public boolean add(double value) {
+		return add(new DoubleValue(value, getEndTime()));
+	}
+
+	public void sort() {
+    	Comparator<Value> comparator = new SortTime();
+		sort(comparator);
+	}
+
+	private class SortTime implements Comparator<Value> {
+		@Override
+		public int compare(Value v1, Value v2) {
+			return (int) (v1.getTimestamp() - v2.getTimestamp());
+		}
+    }
 }

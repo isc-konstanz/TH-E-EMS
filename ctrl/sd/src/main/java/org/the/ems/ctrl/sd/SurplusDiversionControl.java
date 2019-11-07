b3634cfb6ac99974c6eac99f1d9a81a7460cd909
Adrian Minde <Adrian_Minde@live.de>
2019-11-07 12:08:56 +0100

    Implement control lifecycle methods

--- ctrl/sd/src/main/java/org/the/ems/ctrl/sd/SurplusDiversionControl.java
+++ ctrl/sd/src/main/java/org/the/ems/ctrl/sd/SurplusDiversionControl.java
@@ -3,11 +3,7 @@ package org.the.ems.ctrl.sd;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.ServiceScope;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.the.ems.core.ComponentException;
-import org.the.ems.core.EnergyManagementException;
-import org.the.ems.core.GeneratorState;
 import org.the.ems.core.HeatingService;
 import org.the.ems.core.config.Configuration;
 import org.the.ems.core.config.Configurations;
@@ -24,68 +20,74 @@ import org.the.ems.ctrl.tp.TwoPointControl;
 	configurationPolicy = ConfigurationPolicy.REQUIRE
 )
 public class SurplusDiversionControl extends TwoPointControl {
-	private static final Logger logger = LoggerFactory.getLogger(SurplusDiversionControl.class);
 
-	@Configuration(scale=1000, mandatory=false)
-	protected double exportMin = Double.POSITIVE_INFINITY;
+	@Configuration(mandatory=false, scale=1000)
+	protected double surplus = Double.POSITIVE_INFINITY;
+
+	@Configuration(mandatory=false, scale=1000)
+	protected double shortage = 0;
 
 	@Configuration
-	protected ChannelListener export;
+	protected ChannelListener power;
+
+	@Configuration(mandatory=false)
+	protected double powerScale = 1;
+
+	protected double powerValue = Double.NaN;
 
 	@Override
 	public void onActivate(Configurations configs) throws ComponentException {
 		super.onActivate(configs);
 		
-		if (exportMin == Double.POSITIVE_INFINITY && component instanceof HeatingService) {
-			exportMin = ((HeatingService) component).getMinPower();
+		if (surplus == Double.POSITIVE_INFINITY && component instanceof HeatingService) {
+			surplus = ((HeatingService) component).getMinPower();
 		}
-		export.registerValueListener(new PowerListener());
+		power.registerValueListener(new PowerListener());
 	}
 
 	@Override
 	public void onDeactivate() throws ComponentException {
 		super.onDeactivate();
 		
-		export.deregister();
+		power.deregister();
 	}
 
-	protected void onPowerSurplus(double value) {
-		if (component instanceof HeatingService) {
-			HeatingService heating = (HeatingService) component;
-			try {
-				if (temperatureValue <= temperatureMax) {
-					// TODO: implement PID control for dynamic diversion
-					heating.start(new DoubleValue(exportMin));
-				}
-			} catch (EnergyManagementException e) {
-				logger.warn("Unable to start heating \"{}\"", key);
-			}
+	@Override
+	protected boolean checkStart(HeatingService heating) {
+		if (checkMinTemperature()) {
+			return true;
 		}
+		else if (checkMaxTemperature()) {
+			return powerValue >= surplus;
+		}
+		return false;
 	}
 
-	protected void onPowerShortage() {
-		if (component instanceof HeatingService) {
-			HeatingService heating = (HeatingService) component;
-			try {
-				if (heating.getState() == GeneratorState.RUNNING && heating.getRuntime() >= heating.getMinRuntime()) {
-					heating.stop();
-				}
-			} catch (EnergyManagementException e) {
-				logger.warn("Unable to stop heating \"{}\"", key);
-			}
+	@Override
+	protected boolean checkStop(HeatingService heating) {
+		if (checkMaxTemperature()) {
+			return true;
 		}
+		else if (checkMinTemperature()) {
+			return powerValue <= shortage;
+		}
+		return false;
+	}
+
+	@Override
+	protected Value getValue(HeatingService heating) {
+		double value = Math.max(
+			Math.min(powerValue, heating.getMaxPower()), heating.getMinPower()
+		);
+		return new DoubleValue(value);
 	}
 
 	private class PowerListener implements ValueListener {
 
 		@Override
 		public void onValueReceived(Value value) {
-			if (value.doubleValue() >= exportMin) {
-				onPowerSurplus(value.doubleValue());
-			}
-			else if (value.doubleValue() < 0) {
-				onPowerShortage();
-			}
+			powerValue = value.doubleValue()*powerScale;
+			onControlHeating();
 		}
 	}
 

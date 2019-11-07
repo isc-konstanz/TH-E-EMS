fe52c69ff77e5d7ccfeda65f47f3ef5b5d5238a5
Adrian Minde <Adrian_Minde@live.de>
2019-11-07 12:08:03 +0100

    Implement control lifecycle methods

--- ctrl/2p/src/main/java/org/the/ems/ctrl/tp/TwoPointControl.java
+++ ctrl/2p/src/main/java/org/the/ems/ctrl/tp/TwoPointControl.java
@@ -3,16 +3,11 @@ package org.the.ems.ctrl.tp;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.ServiceScope;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.the.ems.core.ComponentException;
-import org.the.ems.core.EnergyManagementException;
-import org.the.ems.core.GeneratorState;
 import org.the.ems.core.HeatingService;
 import org.the.ems.core.config.Configuration;
 import org.the.ems.core.config.Configurations;
 import org.the.ems.core.data.ChannelListener;
-import org.the.ems.core.data.DoubleValue;
 import org.the.ems.core.data.Value;
 import org.the.ems.core.data.ValueListener;
 import org.the.ems.ctrl.Control;
@@ -24,18 +19,17 @@ import org.the.ems.ctrl.Control;
 	configurationPolicy = ConfigurationPolicy.REQUIRE
 )
 public class TwoPointControl extends Control {
-	private static final Logger logger = LoggerFactory.getLogger(TwoPointControl.class);
 
 	@Configuration("temp_min")
-	protected double temperatureMin;
+	private double temperatureMin;
 
 	@Configuration("temp_max")
-	protected double temperatureMax;
+	private double temperatureMax;
 
 	@Configuration("temp")
-	protected ChannelListener temperature;
+	private ChannelListener temperature;
 
-	protected double temperatureValue = Double.NaN;
+	private double temperatureValue = Double.NaN;
 
 	@Override
 	public void onActivate(Configurations configs) throws ComponentException {
@@ -51,30 +45,22 @@ public class TwoPointControl extends Control {
 		temperature.deregister();
 	}
 
-	protected void onTemperatureHigh() {
-		if (component instanceof HeatingService) {
-			HeatingService heating = (HeatingService) component;
-			try {
-				if (heating.getState() == GeneratorState.STANDBY) {
-					heating.start(new DoubleValue(heating.getMaxPower()));
-				}
-			} catch (EnergyManagementException e) {
-				logger.warn("Unable to start heating \"{}\"", key);
-			}
-		}
+	@Override
+	protected boolean checkStart(HeatingService heating) {
+		return checkMinTemperature();
 	}
 
-	protected void onTemperatureLow() {
-		if (component instanceof HeatingService) {
-			HeatingService heating = (HeatingService) component;
-			try {
-				if (heating.getState() == GeneratorState.RUNNING && heating.getRuntime() >= heating.getMinRuntime()) {
-					heating.stop();
-				}
-			} catch (EnergyManagementException e) {
-				logger.warn("Unable to stop heating \"{}\"", key);
-			}
-		}
+	protected boolean checkMinTemperature() {
+		return temperatureValue <= temperatureMax;
+	}
+
+	@Override
+	protected boolean checkStop(HeatingService heating) {
+		return checkMaxTemperature();
+	}
+
+	protected boolean checkMaxTemperature() {
+		return temperatureValue >= temperatureMax;
 	}
 
 	private class TemperatureListener implements ValueListener {
@@ -82,12 +68,8 @@ public class TwoPointControl extends Control {
 		@Override
 		public void onValueReceived(Value value) {
 			temperatureValue = value.doubleValue();
-			if (temperatureValue <= temperatureMin) {
-				onTemperatureHigh();
-			}
-			else if (temperatureValue >= temperatureMax) {
-				onTemperatureLow();
-			}
+			onControlHeating();
 		}
 	}
+
 }
--- ctrl/src/main/java/org/the/ems/ctrl/Control.java
+++ ctrl/src/main/java/org/the/ems/ctrl/Control.java
@@ -1,17 +1,25 @@
 package org.the.ems.ctrl;
 
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.the.ems.core.Component;
 import org.the.ems.core.ComponentException;
 import org.the.ems.core.ComponentService;
 import org.the.ems.core.ComponentType;
+import org.the.ems.core.EnergyManagementException;
 import org.the.ems.core.EnergyManagementService;
+import org.the.ems.core.HeatingService;
 import org.the.ems.core.UnknownComponentException;
 import org.the.ems.core.config.Configuration;
 import org.the.ems.core.config.ConfigurationException;
 import org.the.ems.core.config.Configurations;
+import org.the.ems.core.data.DoubleValue;
+import org.the.ems.core.data.Value;
 
 public abstract class Control extends Component {
+	private final static Logger logger = LoggerFactory.getLogger(Control.class);
+
 	public final static String PID = "org.the.ems.ctrl";
 
 	protected ComponentService component;
@@ -27,12 +35,69 @@ public abstract class Control extends Component {
 	@Override
 	public void onActivate(Configurations configs, BundleContext context) throws ComponentException {
 		super.onActivate(configs, context);
+		logger.info("Activating TH-E EMS {}: {}", this.getClass().getSimpleName(), getId());
 		try {
 			component = context.getService(context.getServiceReference(EnergyManagementService.class))
 					.getComponent(key);
 		} catch (UnknownComponentException e) {
 			throw new ConfigurationException("Unable to find controllable component: " + key);
 		}
+	}
+
+	protected final void onControlHeating() {
+		if (component instanceof HeatingService) {
+			HeatingService heating = (HeatingService) component;
+			try {
+				switch(heating.getState()) {
+				case STANDBY:
+				case STOPPING:
+					if (checkStart(heating)) {
+						doStart(heating);
+						onStart(heating);
+					}
+					break;
+				case RUNNING:
+				case STARTING:
+					if (checkStop(heating) && heating.getRuntime() >= heating.getMinRuntime()) {
+						doStop(heating);
+						onStop(heating);
+					}
+					break;
+				}
+			} catch (EnergyManagementException e) {
+				logger.warn("Unable to control heating \"{}\": {}", key, e.getMessage());
+			}
+		}
+	}
+
+	protected boolean checkStart(HeatingService heating) {
+		// Default implementation to be overridden
+		return true;
+	}
+
+	protected void doStart(HeatingService heating) throws EnergyManagementException {
+		heating.start(getValue(heating));
+	}
+
+	protected Value getValue(HeatingService heating) {
+		return new DoubleValue(heating.getMaxPower());
+	}
+
+	protected void onStart(HeatingService heating) throws EnergyManagementException {
+		// Default implementation to be overridden
+	}
+
+	protected boolean checkStop(HeatingService heating) {
+		// Default implementation to be overridden
+		return true;
+	}
+
+	protected void doStop(HeatingService heating) throws EnergyManagementException {
+		heating.stop();
+	}
+
+	protected void onStop(HeatingService heating) throws EnergyManagementException {
+		// Default implementation to be overridden
 	}
 
 }

ac9c48e562f317e2c0a6945b4fa18fcc9a4441e0
Adrian Minde <Adrian_Minde@live.de>
2020-03-12 15:13:19 +0100

    Reduce TES temp avg interval

--- cmpt/tes/src/main/java/org/the/ems/cmpt/tes/ThermalEnergyStorage.java
+++ cmpt/tes/src/main/java/org/the/ems/cmpt/tes/ThermalEnergyStorage.java
@@ -6,7 +6,6 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -30,6 +29,7 @@ import org.the.ems.core.data.Channel;
 import org.the.ems.core.data.ChannelCollection;
 import org.the.ems.core.data.DoubleValue;
 import org.the.ems.core.data.Value;
+import org.the.ems.core.data.ValueListener;
 import org.the.ems.core.schedule.NamedThreadFactory;
 
 @org.osgi.service.component.annotations.Component(
@@ -38,15 +38,15 @@ import org.the.ems.core.schedule.NamedThreadFactory;
 	configurationPid = ThermalEnergyStorageService.PID,
 	configurationPolicy = ConfigurationPolicy.REQUIRE
 )
-public class ThermalEnergyStorage extends Component implements ThermalEnergyStorageService, Runnable {
-	private static final Logger logger = LoggerFactory.getLogger(ThermalEnergyStorage.class);
+public class ThermalEnergyStorage extends Component 
+		implements ThermalEnergyStorageService, ValueListener, Runnable {
 
-	private static final String TEMP = "temp";
+	private static final Logger logger = LoggerFactory.getLogger(ThermalEnergyStorage.class);
 
 	protected ScheduledExecutorService executor;
 
-	@Configuration(mandatory=false)
-	protected int interval = 15;
+	@Configuration(mandatory=false, scale=60000) // Default interval of 15 minutes
+	protected int interval = 900000;
 
 	// The specific heat capacity of the storage medium. Default is 4.1813 of water.
 	@Configuration(mandatory=false)
@@ -61,13 +61,17 @@ public class ThermalEnergyStorage extends Component implements ThermalEnergyStor
 
 	protected double mass;
 
-	@Configuration(value="heating*")
+	@Configuration(value="heating_*")
 	protected List<String> heatings;
 
-	@Configuration(value="temp*")
+	@Configuration(value="temp_*")
 	protected ChannelCollection temperatures;
 
+	@Configuration(value="temp")
+	protected Channel temperature;
+
 	protected Value temperatureLast = null;
+	protected long timestampLast = -1;
 
 	@Configuration
 	protected Channel power;
@@ -92,6 +96,9 @@ public class ThermalEnergyStorage extends Component implements ThermalEnergyStor
 					energyValues.add(energy);
 				}
 			}
+			for (Channel temperature : temperatures.values()) {
+				temperature.registerValueListener(this);
+			}
 			
 			// Storage medium mass in kilogram
 			mass = capacity*density;
@@ -104,7 +111,7 @@ public class ThermalEnergyStorage extends Component implements ThermalEnergyStor
 			logger.debug("Starting TH-E EMS {} power calculation {}", getId().toUpperCase(), next);
 			
 			executor.scheduleAtFixedRate(this, 
-					time.until(next, ChronoUnit.MILLIS), interval*60000, TimeUnit.MILLISECONDS);
+					time.until(next, ChronoUnit.MILLIS), interval, TimeUnit.MILLISECONDS);
 			
 		} catch (UnknownComponentException e) {
 			throw new ConfigurationException("Unable to find controllable component " + e.getMessage());
@@ -121,41 +128,34 @@ public class ThermalEnergyStorage extends Component implements ThermalEnergyStor
 	public void run() {
 		try {
 			long time = System.currentTimeMillis();
-			Value temperature;
-			if (temperatures.size() > 1) {
-				double tempSum = 0;
-				for (Entry<String, Channel> channel : temperatures.entrySet()) {
-					if (!channel.getKey().equals(TEMP)) {
-						Value temp = channel.getValue().getLatestValue();
-						if (temp != null) {
-							tempSum += temp.doubleValue();
-						}
-					}
-				}
-				temperature = new DoubleValue(tempSum/(temperatures.size() - 1), time);
-				temperatures.get(TEMP).setLatestValue(temperature);
+			double temperature;
+			if (temperatures.size() > 0) {
+				temperature = getAverageTemperature();
 			}
 			else {
-				temperature = temperatures.get(TEMP).getLatestValue();
+				Value temp = getTemperature();
+				if (temp == null) {
+					return;
+				}
+				temperature = temp.doubleValue();
 			}
-			
-			if (temperature != null && temperatureLast != null) {
+			double energyInput = 0;
+			for (HeatingEnergy energy : energyValues) {
+				energyInput += energy.getValue().doubleValue();
+			}
+			if (temperatureLast != null) {
 				long timeDelta = (time - temperatureLast.getTime())/1000;
-				double tempDelta = temperatureLast.doubleValue() - temperature.doubleValue();
+				double tempDelta = temperatureLast.doubleValue() - temperature;
 				
 				// Calculate energy in Q[kJ] = cp*m[kg]*dT[°C]
 				double energyDelta = specificHeat*mass*tempDelta;
-				
-				for (HeatingEnergy energy : energyValues) {
-					energyDelta += energy.getValue().doubleValue();
-				}
-				energyDelta = Math.max(energyDelta, 0);
+				energyDelta = Math.max(energyDelta + energyInput, 0);
 				
 				energyLast += energyDelta/3600;
-				energy.setLatestValue(new DoubleValue(energyLast, temperature.getTime()));
-				power.setLatestValue(new DoubleValue(energyDelta/timeDelta, temperature.getTime()));
+				energy.setLatestValue(new DoubleValue(energyLast, time));
+				power.setLatestValue(new DoubleValue(energyDelta/timeDelta, time));
 			}
-			temperatureLast = temperature;
+			temperatureLast = new DoubleValue(temperature, time);
 			
 		} catch (Exception e) {
 			logger.warn("Error calculating power: {}", e.getMessage());
@@ -173,15 +173,44 @@ public class ThermalEnergyStorage extends Component implements ThermalEnergyStor
 	}
 
 	@Override
-	@Configuration(TEMP)
 	public Value getTemperature() throws ComponentException {
-		return getConfiguredValue(TEMP);
+		return temperature.getLatestValue();
+	}
+
+	protected double getAverageTemperature() {
+		int tempCount = 0;
+		double tempSum = 0;
+		for (Channel channel : temperatures.values()) {
+			Value temp = channel.getLatestValue();
+			if (temp != null) {
+				tempSum += temp.doubleValue();
+				tempCount++;
+			}
+		}
+		return tempSum/tempCount;		
+	}
+
+	@Override
+	public void onValueReceived(Value value) {
+		long timeMax = -1;
+		for (Channel channel : temperatures.values()) {
+			Value temp = channel.getLatestValue();
+			if (temp == null || temp.getTime() <= timestampLast) {
+				return;
+			}
+			else if (temp.getTime() > timeMax) {
+				timeMax = temp.getTime();
+			}
+		}
+		timestampLast = timeMax;
+		temperature.setLatestValue(new DoubleValue(getAverageTemperature(), timestampLast));
 	}
 
 	private TemporalAdjuster next(int interval) {
+		int minutes = interval/60000;
 		return (temporal) -> {
 			int minute = temporal.get(ChronoField.MINUTE_OF_DAY);
-			int next = (minute / interval + 1) * interval;
+			int next = (minute / minutes + 1) * minutes;
 			return temporal.with(ChronoField.SECOND_OF_DAY, 0)
 					.with(ChronoField.MILLI_OF_DAY, 0)
 					.with(ChronoField.NANO_OF_DAY, 0)

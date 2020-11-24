b5120535be7eaa4f8fb22b96ccb6341f083b6e02
Adrian Minde <Adrian_Minde@live.de>
2020-11-24 15:20:09 +0100

    Fix peak shaving control and deactivate PID again

--- ctrl/ps/src/main/java/org/the/ems/ctrl/ps/PeakShavingControl.java
+++ ctrl/ps/src/main/java/org/the/ems/ctrl/ps/PeakShavingControl.java
@@ -1,5 +1,7 @@
 package org.the.ems.ctrl.ps;
 
+import static java.lang.Math.abs;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -28,7 +30,7 @@ public class PeakShavingControl extends TwoPointControl {
 	@Configuration(mandatory=false, scale=-1000)
 	protected double exportMax = Double.NEGATIVE_INFINITY;
 
-	@Configuration(mandatory=false, scale=-1000)
+	@Configuration(mandatory=false, scale=1000)
 	protected double exportHyst = 0;
 
 
@@ -52,7 +54,7 @@ public class PeakShavingControl extends TwoPointControl {
 	protected double controlDerivative = 0;
 
 	@Configuration(mandatory=false, section="PID", value="tolerance")
-	protected double controlTolerance = 50;
+	protected double controlTolerance = 10;
 
 	@Configuration(mandatory=false, section="PID", value="maximum")
 	protected double controlMax = Double.POSITIVE_INFINITY;
@@ -86,17 +88,19 @@ public class PeakShavingControl extends TwoPointControl {
 
 	protected void set(Value value) {
 		double power = value.doubleValue();
-		long seconds = (value.getTime() - powerValue.getTime())/1000;
-		if (seconds < 1) {
-			seconds = 1;
-		}
+//		long seconds = (value.getTime() - powerValue.getTime())/1000;
+//		if (seconds < 1) {
+//			seconds = 1;
+//		}
 		double error = 0;
 		
 		if (power <= exportMax) {
 			error = exportMax - power;
+			logger.trace("Power export above {}: {}", abs(exportMax), power);
 		}
 		if (power > importMax) {
 			error = importMax - power;
+			logger.trace("Power import above {}: {}", abs(importMax), power);
 		}
 		
 		if (error != 0 &&
@@ -106,18 +110,23 @@ public class PeakShavingControl extends TwoPointControl {
 			controlValue = 0;
 			controlError = 0;
 			controlErrorIntegral = 0;
+			
+			logger.debug("Power boundary infringement changed extreme. Control values will be resetted.");
 		}
 		else if (controlValue > 0) {
 			error = exportMax - power;
+			logger.trace("Power export error from {}: {}", abs(exportMax), error);
 		}
 		else if (controlValue < 0) {
 			error = importMax - power;
+			logger.trace("Power import error from {}: {}", abs(importMax), error);
 		}
+//		double controlErrorProportional = error*controlProportional;
+//		double controlErrorDerivate = (error - controlError)/seconds*controlDerivative;
+//		controlErrorIntegral += error*seconds*controlIntegral;
+//		controlError = error;
 		
-		double controlErrorProportional = error*controlProportional;
-		double controlErrorDerivate = (error - controlError)/seconds*controlDerivative;
-		controlErrorIntegral += error*seconds*controlIntegral;
-		controlError = error;
+		controlErrorIntegral += error;
 		
 		if (controlErrorIntegral > controlMax) {
 			controlErrorIntegral = controlMax;
@@ -125,43 +134,51 @@ public class PeakShavingControl extends TwoPointControl {
 		if (controlErrorIntegral < -controlMax) {
 			controlErrorIntegral = -controlMax;
 		}
-		double controlValue = controlErrorProportional + controlErrorIntegral + controlErrorDerivate;
-		if (Math.abs(this.controlValue) < controlTolerance && 
-				Math.abs(controlValue) < controlTolerance &&
+//		TODO: Verify PID control functionality and uncomment again together with commented lines above
+//		double controlValue = controlErrorProportional + controlErrorIntegral + controlErrorDerivate;
+		double controlValue = controlErrorIntegral;
+		if ((abs(this.controlValue) < controlTolerance && 
+				abs(controlValue) < controlTolerance) ||
 				(this.controlValue > 0 && controlValue < 0) ||
 				(this.controlValue < 0 && controlValue > 0)) {
 			
 			controlValue = 0;
 			controlError = 0;
 			controlErrorIntegral = 0;
+			
+			if (this.controlValue != 0) {
+				logger.debug("Power boundary infringement resolved.");
+			}
 		}
-		if (controlValue > controlMax) {
+		else if (controlValue > controlMax) {
 			controlValue = controlMax;
 		}
 		else if (controlValue < -controlMax) {
 			controlValue = -controlMax;
 		}
 		this.controlValue = controlValue;
-		
-		this.inverters.set(
-				new DoubleValue(controlValue, value.getTime()));
+		if (controlValue != 0) {
+			logger.trace("Power control value update: {}", controlValue);
+		}
+		this.inverters.set(new DoubleValue(controlValue, value.getTime()));
 	}
 
 	protected void onPowerChanged(Value value) {
 		double power = value.doubleValue();
 		
-		if ((power > importMax && powerValue.doubleValue() <= importMax) || 
-				(power > exportMax + exportHyst &&
-				heatings.hasStoppable(ComponentType.HEATING_ROD, ComponentType.HEAT_PUMP))) {
+		if ((power > importMax || power > exportMax + exportHyst) &&
+				heatings.hasStoppable(ComponentType.HEATING_ROD, ComponentType.HEAT_PUMP)) {
 			
 			heatings.stopFirst(ComponentType.HEATING_ROD, ComponentType.HEAT_PUMP);
+			logger.debug("Stopping heating due to power boundary infringement: {}", power);
 		}
-		else if (power <= exportMax && powerValue.doubleValue() <= exportMax &&
-				heatings.hasStartable(ComponentType.HEAT_PUMP, ComponentType.HEATING_ROD)) {
+		else if (power <= exportMax && 
+				power + heatings.getStartableMinPower(ComponentType.HEAT_PUMP, ComponentType.HEATING_ROD) <= exportMax + exportHyst) {
 			
 			heatings.startFirst(ComponentType.HEAT_PUMP, ComponentType.HEATING_ROD);
+			logger.debug("Starting heating due to power boundary infringement: {}", power);
 		}
-		else if (power <= exportMax || power > importMax || controlValue != 0) {
+		else {
 			set(value);
 		}
 	}
